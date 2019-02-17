51826775078a0be5f8cb1d1d5d61495b125629f9
Masayuki Keida <masayuki.keida+github@gmail.com>
2019-02-17 16:49:42 +0900

    second commit

---
+++ lda.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(topicmodels)
+library(tm)
+library(lda)
+
+#docnum <- 55;
+docnum <- 70;
+
+res3 <- read.table("resource.txt", header=T);
+res4 <- res3;
+rownames(res4) <- res4$TERM;
+res4 <- res4 %>% select(-c(TERM:POS2));
+res5 <- as.DocumentTermMatrix(t(res4), weighting= weightTf);
+k <- 3;
+model <- LDA(res5, k);
+terms(model);
+
+res6 <- topicmodels::dtm2ldaformat(res5);
+set.seed(123);
+result <- lda::lda.collapsed.gibbs.sampler(res6$documents, K = 3, res6$vocab, 200000, 0.1, 0.1, compute.log.likelihood=T);
+top.words <- lda::top.topic.words(result$topics, 20, by.score=T);
+print(top.words);
+write.table(top.words, "top_words.txt", col.names=F, row.names=F, sep="\t", append=F);
+
+topic.proportions <- t(result$document_sums) / colSums(result$document_sums);
+topic.proportions <- as.data.frame(topic.proportions);
+rownames(topic.proportions) <- colnames(res4);
+colnames(topic.proportions) <- paste("topic", 1:3);
+write.table(topic.proportions, "topic_proportions.txt", col.names=T, row.names=T, sep="\t", append=F);
---
+++ process0.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(RMeCab)
+
+#docnum <- 55;
+docnum <- 70;
+
+res0 <- docDF("./text", type=1, N=1);
+res1 <- res0 %>% filter(POS1 %in% c("名詞", "動詞")) %>% filter(POS2 %in% c("サ変接続", "一般", "形容動詞語幹", "固有名詞", "自立", "数"));
+
+res1_dup_terms <- res1[duplicated(res1$TERM),1];
+
+for (i in 1:length(res1_dup_terms)) {
+    duprows <- grep(paste("^", res1_dup_terms[i], "$", sep=""), res1$TERM);
+    t_TERM <- unique(res1[duprows, 1]);
+    if (length(t_TERM) > 1) {
+        t_TERM <- paste(unique(res1[duprows, 1]), collapse="+");
+    }
+    t_POS1 <- unique(res1[duprows, 2]);
+    if (length(t_POS1) > 1) {
+        t_POS1 <- paste(unique(res1[duprows, 2]), cpllapse="+");
+    }
+    t_POS2 <- unique(res1[duprows, 3]);
+    if (length(t_POS2) > 1) {
+        t_POS2 <- paste(unique(res1[duprows, 3]), collapse="+");
+    }
+    newline <- apply(res1[duprows, 4:docnum+3], 2, sum);
+
+    for (i in duprows) {
+        res1[i, 1] <- t_TERM;
+        res1[i, 2] <- t_POS1;
+        res1[i, 3] <- t_POS2;
+        res1[i, 4:docnum+3] <- newline;
+    }
+}
+
+res2 <- res1 %>% distinct(TERM, .keep_all=T);
+
+write.table(res2, "./resource.txt", col.names=T, row.names=F, sep="\t", quote=F, append=F);

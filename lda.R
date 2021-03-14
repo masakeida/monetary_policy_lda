e91e9d53a801ec4133f0142b473b88e0c26be947
Masayuki Keida <masayuki.keida+github@gmail.com>
2021-03-14 13:29:15 +0000

    refine and delete magic number

--- lda.R
+++ lda.R
@@ -12,19 +12,20 @@ rownames(res4) <- res4$TERM;
 res4 <- res4 %>% select(-c(TERM:POS2));
 res5 <- as.DocumentTermMatrix(t(res4), weighting= weightTf);
 k <- 3;
+numtopw <- 20;  # number of top words in topic
 model <- LDA(res5, k);
 terms(model);
 
 res6 <- topicmodels::dtm2ldaformat(res5);
 # comment out the line below, if you need the randomness.
 set.seed(123);
-result <- lda::lda.collapsed.gibbs.sampler(res6$documents, K = 3, res6$vocab, 200000, 0.1, 0.1, compute.log.likelihood=T);
-top.words <- lda::top.topic.words(result$topics, 20, by.score=T);
+result <- lda::lda.collapsed.gibbs.sampler(res6$documents, K = k, res6$vocab, 200000, 0.1, 0.1, compute.log.likelihood=T);
+top.words <- lda::top.topic.words(result$topics, numtopw, by.score=T);
 print(top.words);
 write.table(top.words, "top_words.txt", col.names=F, row.names=F, sep="\t", append=F);
 
 topic.proportions <- t(result$document_sums) / colSums(result$document_sums);
 topic.proportions <- as.data.frame(topic.proportions);
 rownames(topic.proportions) <- colnames(res4);
-colnames(topic.proportions) <- paste("topic", 1:3);
+colnames(topic.proportions) <- paste("topic", 1:k);
 write.table(topic.proportions, "topic_proportions.txt", col.names=T, row.names=T, sep="\t", append=F);
